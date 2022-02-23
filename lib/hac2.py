576e5fce4f45068aa97bc202227873a7097730f9
Rafael Alvarado <ontoligent@gmail.com>
2022-02-23 17:46:31 -0500

    Renamed variable

--- lib/hac2.py
+++ lib/hac2.py
@@ -9,22 +9,22 @@ class HAC:
     should be sufficiently small to allow being plotted."""
 
     w:int = 10
-    labelsize:int = 14
+    label_size:int = 14
     orientation:str = 'left'
     dist_metric:str = 'cosine' # ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulsinski’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’
     linkage_method:str = 'ward'
-    norm:str = 'l2' # l1, l2, max
+    norm_type:str = 'l2' # l1, l2, max
     
-    def __init__(self, M, labels=None):
-        self.M = M
-        self.h = M.shape[0]
+    def __init__(self, X, labels=None):
+        self.X = X
+        self.h = X.shape[0]
         if labels:
             self.labels = labels            
         else:
-            self.labels = M.index.tolist()
+            self.labels = X.index.tolist()
 
     def get_sims(self):
-        self.SIMS = pdist(normalize(self.M, norm=self.norm), metric=self.dist_metric)
+        self.SIMS = pdist(normalize(self.X, norm=self.norm_type), metric=self.dist_metric)
 
     def get_tree(self):
         self.TREE = sch.linkage(self.SIMS, method=self.linkage_method)        
@@ -33,7 +33,7 @@ class HAC:
         plt.figure()
         plt.subplots(figsize=(self.w, self.h / 3))
         sch.dendrogram(self.TREE, labels=self.labels, orientation=self.orientation);
-        plt.tick_params(axis='both', which='major', labelsize=self.labelsize)
+        plt.tick_params(axis='both', which='major', labelsize=self.label_size)
         
     def plot(self):
         self.get_sims()
