00cc8fa9ccff53d64bb759dbf0d5572ede6868ca
Rafael Alvarado <ontoligent@gmail.com>
2022-02-24 09:57:44 -0500

    Changed default distance measure to euclidean

--- lib/hac2.py
+++ lib/hac2.py
@@ -1,7 +1,8 @@
 import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import pdist
 from sklearn.preprocessing import normalize
-import matplotlib.pyplot as plt            
+import matplotlib.pyplot as plt
+from pandas import DataFrame as df
 
 class HAC:
     """This class takes an arbitrary vector space and represents it 
@@ -11,9 +12,10 @@ class HAC:
     w:int = 10
     label_size:int = 14
     orientation:str = 'left'
-    dist_metric:str = 'cosine' # ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulsinski’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’
+    dist_metric:str = 'euclidean' # ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulsinski’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’
     linkage_method:str = 'ward'
     norm_type:str = 'l2' # l1, l2, max
+    color_thresh:float = None
     
     def __init__(self, X, labels=None):
         self.X = X
@@ -30,9 +32,17 @@ class HAC:
         self.TREE = sch.linkage(self.SIMS, method=self.linkage_method)        
         
     def plot_tree(self):
+        if not self.color_thresh:
+            self.color_thresh = df(self.TREE)[2].median()
         plt.figure()
         plt.subplots(figsize=(self.w, self.h / 3))
-        sch.dendrogram(self.TREE, labels=self.labels, orientation=self.orientation);
+        sch.dendrogram(self.TREE, 
+            labels=self.labels, 
+            orientation=self.orientation,
+            count_sort=True,
+            distance_sort=True,
+            above_threshold_color='.75',
+            color_threshold=self.color_thresh);
         plt.tick_params(axis='both', which='major', labelsize=self.label_size)
         
     def plot(self):
