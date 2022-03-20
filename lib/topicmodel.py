1c09a2e6e7e2037680c22d4ca23df0622c6eb201
Rafael Alvarado <ontoligent@gmail.com>
2022-03-20 12:36:39 -0400

    Improved TOPIC info

--- lib/topicmodel.py
+++ lib/topicmodel.py
@@ -6,7 +6,7 @@ class TopicModel():
 
     bow_count_col:str = 'n'
     n_topics:int = 20
-    n_top_terms:int = 7
+    n_top_terms:int = None
     engine_type = 'LDA' # Also NMF
 
     # LDA Params
@@ -22,6 +22,7 @@ class TopicModel():
         self.BOW = BOW
         
     def create_X(self):
+        
         X = self.BOW[self.bow_count_col].unstack()
         V = X.count().to_frame('df')
         X = X.fillna(0)
@@ -49,24 +50,35 @@ class TopicModel():
                 
         self.THETA = pd.DataFrame(self.engine.fit_transform(self.X.values), index=self.X.index)
         self.THETA.columns.name = 'topic_id'
+        
         self.PHI = pd.DataFrame(self.engine.components_, columns=self.X.columns)
         self.PHI.index.name = 'topic_id'
         self.PHI.columns.name = 'term_str'
+        
+        self.TOPIC = self.PHI.sum(1).to_frame('phi_sum')
+        self.TOPIC['theta_sum'] = self.THETA.sum()
 
     def describe_topics(self):
-        self.TOPIC = self.PHI.stack().to_frame('topic_weight')\
+        
+        # Compute topic entropy over PHI to get n for top terms
+        PHI_P = (self.PHI.T / self.PHI.T.sum())
+        PHI_I = np.log2(1/PHI_P)
+        self.TOPIC['h'] = round((PHI_I * PHI_P).sum().sort_values(ascending=False), 2)
+        if not self.n_top_terms:
+            self.n_top_terms = round(self.TOPIC.h.mean())
+
+        self.TOPIC['top_terms'] = self.PHI.stack().to_frame('topic_weight')\
             .groupby('topic_id')\
             .apply(lambda x: 
-                x.sort_values('topic_weight', ascending=False)\
-                   .head(n_top_terms)\
-                   .reset_index()\
-                   .drop('topic_id',1)['term_str'])
-        self.TOPIC['label'] = self.TOPIC.apply(lambda x: str(x.name) + ' ' + ', '.join(x[:self.n_top_terms]), 1)
-        self.TOPIC['doc_weight_sum'] = self.THETA.sum()
+                x.sort_values('topic_weight', ascending =False)\
+                    .head(self.n_top_terms)\
+                    .reset_index()['term_str'])\
+            .apply(lambda x: ' '.join(x.replace(' ', '_')), 1)
+        
+        self.TOPIC['label'] = self.TOPIC.apply(lambda x: f"{x.name}: {x.top_terms}", 1)
                 
     def get_model_stats(self):
-        p = self.TOPIC.doc_weight_sum / self.TOPIC.doc_weight_sum.sum()
-        self.entropy = (p * np.log2(1/p)).sum()
+        self.entropy = self.TOPIC.h.sum()
         self.redundancy = 1 - self.entropy / np.log2(self.n_topics)
     
     def get_doc_stats(self):
@@ -74,5 +86,4 @@ class TopicModel():
         self.DOC['max_topic'] = self.THETA.idxmax(1)
         
     def plot_topics(self):
-        self.TOPIC.sort_values('doc_weight_sum', ascending=True)\
-            .plot.barh(y='doc_weight_sum', x='label', figsize=(5, self.n_topics/2))
+        self.TOPIC.sort_values('theta_sum', ascending=True).plot.barh(y='theta_sum', x='label', figsize=(5, self.n_topics/2))
